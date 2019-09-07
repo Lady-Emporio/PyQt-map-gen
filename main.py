2b0a4239690f3b1379fe663640c63834ae29a8a8
Lady-Emporio <Lady-Emporio>
2019-09-07 20:57:05 +0300

    add log, load/save json from file

--- main.py
+++ main.py
@@ -9,9 +9,87 @@ QSpinBox)
 from PyQt5.QtCore import QObject, pyqtSignal
 from PyQt5.QtWidgets import QApplication,QLayout
 from PyQt5.QtWidgets import QGraphicsScene,QGraphicsEllipseItem,QGraphicsLineItem,QListWidget ,QSizePolicy , QGraphicsRectItem,QListWidgetItem
-from PyQt5.QtCore import QTimer,Qt
+from PyQt5.QtCore import QTimer,Qt,QMimeData
 from PyQt5.QtGui import QColor,QBrush,QPen,QTransform ,QPixmap,QCursor,QIcon
-
+import json
+class Colors():
+	fileName="conf.json"
+	fileError="log.txt"
+	KeyColors="COLORS"
+	def __init__(self):	
+		self.colors=[]
+		
+		self.clearLog()
+		try:
+			with open(Colors.fileName,"r") as file:
+				json_string=file.read()
+				data={}
+				try:
+					data = json.loads(json_string)
+				except Exception  as e:
+					textError=str( type(e) ) +" | " + str(e)
+					self.log(textError)
+				colors=data.get( Colors.KeyColors, None)
+				if None==colors:
+					self.createDefaultValue()
+					return;
+				if type(colors) == list:
+					self.colors=colors
+				else:
+					self.createDefaultValue()
+		except IOError as e:
+			self.createDefaultValue()
+			
+	def createDefaultValue(self):
+		jsonObj={}
+		self.colors=[
+			{"r":50,"g":205,"b":50},
+			{"r":255,"g":20,"b":147},
+			{"r":0,"g":128,"b":0},
+			{"r":128,"g":128,"b":0},
+			{"r":0,"g":128,"b":128},
+			{"r":0,"g":0,"b":139},
+			{"r":138,"g":43,"b":226},
+			{"r":147,"g":112,"b":219},
+			{"r":128,"g":0,"b":128},
+			{"r":75,"g":0,"b":130},
+			{"r":106,"g":90,"b":205},
+			{"r":218,"g":165,"b":32},
+			{"r":205,"g":133,"b":63},
+			{"r":139,"g":69,"b":19},
+			{"r":160,"g":82,"b":45},
+		]
+		jsonObj[Colors.KeyColors]=self.colors
+		
+		
+		with open(Colors.fileName,"w") as file:
+			json_string = json.dumps(jsonObj,indent=4)
+			file.write(json_string)
+			
+	def log(self,textError):
+		with open(Colors.fileError,"w") as file:
+			file.write(textError)
+			
+	def clearLog(self):
+		with open(Colors.fileError,"w") as file:
+			file.write("")
+			
+	def saveColors(self,QColorsList):
+		self.colors=[]
+		
+		for qcolor in QColorsList:
+			c={}
+			c["r"]=qcolor.red()
+			c["g"]=qcolor.green()
+			c["b"]=qcolor.blue()
+			self.colors.append(c)
+			
+		jsonObj={}
+		jsonObj[Colors.KeyColors]=self.colors
+		with open(Colors.fileName,"w") as file:
+			json_string = json.dumps(jsonObj,indent=4)
+			file.write(json_string)
+		
 class Cell(QGraphicsRectItem):
 	def __init__(self,row,col):
 		QGraphicsRectItem.__init__(self)
@@ -108,7 +186,18 @@ class List(QListWidget):
 			self.lastItem=item
 			self.scene.activeColor=item.background().color()
 			
-			
+	def getColors(self):
+	
+		ListWidgetItems =[]
+		QColorsList=[]
+		for i in range(self.count()) :
+			ListWidgetItems.append( self.item(i))
+		for item in ListWidgetItems:
+			QColorsList.append( item.background().color() )
+		
+		print(len(QColorsList))
+		return QColorsList
+		
 class MainWindow(QWidget):
 	def __init__(self):
 		QWidget.__init__(self)
@@ -168,33 +257,39 @@ class MainWindow(QWidget):
 		self.list.setSizePolicy(QSizePolicy.Preferred,QSizePolicy.Maximum)
 		rightLayout.addWidget(self.list,Alignment)
 		
-		colors=[
-			QColor(50, 205, 50),
-			QColor(255, 20, 147),
-			QColor(0, 128, 0),
-			QColor(128, 128, 0),
-			QColor(0, 128, 128),
-			QColor(0, 0, 139),
-			QColor(138, 43, 226),
-			QColor(147, 112, 219),
-			QColor(128, 0, 128),
-			QColor(75, 0, 130),
-			QColor(106, 90, 205),
-			QColor(218, 165, 32),
-			QColor(205, 133, 63),
-			QColor(139, 69, 19),
-			QColor(160, 82, 45),
-		]
-		for color in colors:
+		# colors=[
+			# QColor(50, 205, 50),
+			# QColor(255, 20, 147),
+			# QColor(0, 128, 0),
+			# QColor(128, 128, 0),
+			# QColor(0, 128, 128),
+			# QColor(0, 0, 139),
+			# QColor(138, 43, 226),
+			# QColor(147, 112, 219),
+			# QColor(128, 0, 128),
+			# QColor(75, 0, 130),
+			# QColor(106, 90, 205),
+			# QColor(218, 165, 32),
+			# QColor(205, 133, 63),
+			# QColor(139, 69, 19),
+			# QColor(160, 82, 45),
+		# ]
+		self.colors=Colors();
+		for colorObj in self.colors.colors:
+		
+			color=QColor( colorObj["r"],colorObj["g"],colorObj["b"])
 			item=QListWidgetItem();
 			item.setBackground(color)
 			self.list.addItem(item)
 		
 		
 
-		
+		self.saveColors()
 		clearButton.clicked.connect(self.clear_trigger)    
 		
+	def saveColors(self):
+		colors=self.list.getColors()
+		self.colors.saveColors(colors)
 	def clear_trigger(self):
 		maxRow=self.rowsEdit.value()+2
 		maxColumns=self.columnsEdit.value()+2
